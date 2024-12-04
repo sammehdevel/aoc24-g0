853f1703aa29d66be83faa3da3ee5d950898eb8c
sharrison <sharrison@streamsystems.ca>
2024-12-03 18:02:48 -0700

    day 1 part 1

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-	file, err := os.Open("input.txt")
+	var args = os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Usage: go run main.go <input_file>")
+		os.Exit(1)
+	}
+	file, err := os.Open(args[0])
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		panic(1)
@@ -23,12 +32,32 @@ func main() {
 	}(file)
 
 	scanner := bufio.NewScanner(file)
-	i := 0
+	var left []int
+	var right []int
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Printf("%v %v\n", line, i)
-		i++
+		tokens := strings.Split(line, " ")
+		l, err := strconv.Atoi(tokens[0])
+		if err != nil {
+			panic(1)
+		}
+		left = append(left, l)
+		r, err := strconv.Atoi(tokens[len(tokens)-1])
+		if err != nil {
+			panic(1)
+		}
+		right = append(right, r)
 	}
+
+	slices.Sort(left)
+	slices.Sort(right)
+
+	var d int
+	for i := 0; i < len(left); i++ {
+		d += int(math.Abs(float64(left[i] - right[i])))
+	}
+
+	fmt.Println(d)
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
